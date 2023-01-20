b8ba66ad7d060ac971060d68ac4f0dc040eb0024
Raymond Loman <r.loman@recognize.nl>
2023-01-20 13:08:02 +0100

    Work in progress

--- src/nl/workingspirit/exceptions/App.java
+++ src/nl/workingspirit/exceptions/App.java
@@ -1,15 +1,33 @@
 package nl.workingspirit.exceptions;
 
+import nl.workingspirit.model.Apple;
+import nl.workingspirit.model.Fruit;
+import nl.workingspirit.model.Pear;
+
 public class App {
 
     public static void main(String[] args) {
         int a = 3;
-        int b = 0;
+        int b = 1;
+
+        System.out.println(divide(a, b));
+
 
         try {
             System.out.println(divide(a, b));
-        } catch (WorkingSpiritException e) {
+            Fruit fruit = new Pear();
+//            Apple apple = (Apple) fruit;
+
+            String age = "poedel";
+            int ageAsInt = Integer.parseInt(age);
+            System.out.println(ageAsInt + 2);
+            // multi catch block
+        } catch (WorkingSpiritException | ClassCastException | NumberFormatException e) {
             System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Andere exception afgehandeld");
+        } finally {
+            System.out.println("Dit block wordt ALTIJD uitgevoerd, tenzij je de stroom uitzet of System.exit");
         }
         System.out.println("Einde programma");
     }
