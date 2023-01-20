e6282900ada4283954aa182111a58ccac1f5854c
Raymond Loman <r.loman@recognize.nl>
2023-01-20 15:16:20 +0100

    Add tip

--- src/nl/workingspirit/debugging/App.java
+++ src/nl/workingspirit/debugging/App.java
@@ -2,18 +2,16 @@ package nl.workingspirit.debugging;
 
 public class App {
 
+// Using intellij or eclipse 
+// use click in left margin to enable debug in that line.
+// Run using 'Debug' instead of 'Run' to run the
+// program in debug mode
     public static void main(String[] args) {
         int a = 3;
         int b = 1;
 
         System.out.println(Integer.parseInt("3"));
-
-
-        //
-
         System.out.println(a / b);
-
         System.out.println("einde");
-
     }
 }
