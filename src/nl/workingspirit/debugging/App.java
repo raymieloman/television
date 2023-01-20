7575e5249b652ac6f6d748d1e4f38c7c1a7c5c6b
Raymond Loman <r.loman@recognize.nl>
2023-01-20 15:12:57 +0100

    Add some code for debug

---
+++ src/nl/workingspirit/debugging/App.java
@@ -0,0 +1,19 @@
+package nl.workingspirit.debugging;
+
+public class App {
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 1;
+
+        System.out.println(Integer.parseInt("3"));
+
+
+        //
+
+        System.out.println(a / b);
+
+        System.out.println("einde");
+
+    }
+}
