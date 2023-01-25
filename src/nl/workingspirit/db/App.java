afcf107d45f4817c27ad869ddff7062932b834ed
Raymond Loman <r.loman@recognize.nl>
2023-01-25 10:17:34 +0100

    wip

--- src/nl/workingspirit/db/App.java
+++ src/nl/workingspirit/db/App.java
@@ -1,26 +1,31 @@
 package nl.workingspirit.db;
 
+import java.io.FileReader;
 import java.sql.*;
 
 public class App {
 
     public static void main(String[] args) throws SQLException {
-        String sqlSelectAllPersons = "SELECT * FROM person";
-        String connectionUrl = "jdbc:mysql://localhost:3306/test?serverTimezone=UTC";
 
-        try (Connection conn = DriverManager.getConnection(connectionUrl, "username", "password");
+        String connectionUrl = "jdbc:mysql://localhost:3306/people?serverTimezone=UTC";
+        String sqlSelectAllPersons = "SELECT * FROM person";
+        try (Connection conn = DriverManager.getConnection(connectionUrl, "root", "people");
+
              PreparedStatement ps = conn.prepareStatement(sqlSelectAllPersons);
              ResultSet rs = ps.executeQuery()) {
 
             while (rs.next()) {
                 long id = rs.getLong("ID");
-                String name = rs.getString("FIRST_NAME");
+                String firstName = rs.getString("FIRST_NAME");
                 String lastName = rs.getString("LAST_NAME");
+
+                System.out.println("firstName:"+ firstName);
 
                 // do something with the extracted data...
             }
         } catch (SQLException e) {
             // handle the exception
+            e.printStackTrace();
         }
     }
 }
