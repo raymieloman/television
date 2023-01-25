293bae044e0974dffed06be2288bc1a8d4476a5c
Raymond Loman <r.loman@recognize.nl>
2023-01-25 14:07:13 +0100

    Finish db

--- src/nl/workingspirit/db/App.java
+++ src/nl/workingspirit/db/App.java
@@ -5,21 +5,25 @@ import java.sql.*;
 
 public class App {
 
+    // Very first JDBC example (Java DataBase Connectivity)
     public static void main(String[] args) throws SQLException {
 
         String connectionUrl = "jdbc:mysql://localhost:3306/people?serverTimezone=UTC";
-        String sqlSelectAllPersons = "SELECT * FROM person";
+        String sqlSelectAllPersons = "SELECT id, first_name, last_name FROM person";
         try (Connection conn = DriverManager.getConnection(connectionUrl, "root", "people");
 
-             PreparedStatement ps = conn.prepareStatement(sqlSelectAllPersons);
-             ResultSet rs = ps.executeQuery()) {
+             PreparedStatement preparedStatement = conn.prepareStatement(sqlSelectAllPersons);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
 
-            while (rs.next()) {
-                long id = rs.getLong("ID");
-                String firstName = rs.getString("FIRST_NAME");
-                String lastName = rs.getString("LAST_NAME");
+            while (resultSet.next()) {
+                long id = resultSet.getLong("ID");
+                String firstName = resultSet.getString("FIRST_NAME");
+                String lastName = resultSet.getString("LAST_NAME");
 
-                System.out.println("firstName:"+ firstName);
+                System.out.println("id: "+id+", firstName: "+ firstName+", lastName: "+lastName);
+
+                // met printf
+                System.out.printf("id: %d, firstName: %s, lastname: %s%n", id, firstName, lastName);
 
                 // do something with the extracted data...
             }
